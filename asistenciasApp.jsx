672acb93e46bfe0bc1a4b68a05679a0f90dba3ce
patrickZC21 <zamatapatrick8@gmail.com>
2025-07-26 11:40:07 -0500

    Reorganizar: Mover archivos de frontend-App a la raíz del repositorio

--- asistenciasApp.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import AsistenciasPage from "./src/pages/AsistenciasPage";
-
-const AsistenciasApp = () => {
-  return <AsistenciasPage />;
-};
-
-export default AsistenciasApp;
